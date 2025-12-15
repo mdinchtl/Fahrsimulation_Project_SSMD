6899fb07afe5bcdd5d2c8deb9c8734b0e84a2ac2
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2025-12-15 09:25:31 +0100

    🌸 vorrang regel wird erkannt aber funktioniert noch nicht

--- Assets/Fahrsimulation/Scripts/FollowStreet.cs
+++ Assets/Fahrsimulation/Scripts/FollowStreet.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class FollowStreet : MonoBehaviour
 {
     public TextMeshProUGUI Text;
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Street"))
-        {
-           Text.text = "Du befindest dich nicht mehr auf der Spur !";
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Street"))
+        if (other.CompareTag("Ground"))
         {
-            Text.text = " ";
+            Text.text = "Du befindest dich nicht mehr auf der Spur !";
 
         }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Text.text = "";
+        }
+
     }
 }
