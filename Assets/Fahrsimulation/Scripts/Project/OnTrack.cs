3465dd864045583809d9532e340917bfc79bc1b8
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2026-03-09 09:31:17 +0100

    🌸Auto eigenschaften bearbeitet

---
+++ Assets/Fahrsimulation/Scripts/Project/OnTrack.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class OnTrack : MonoBehaviour
+{
+    public TextMeshProUGUI Text;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Text.text = "Du befindest dich nicht mehr auf der Spur !";
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Text.text = "";
+        }
+    }
+}
