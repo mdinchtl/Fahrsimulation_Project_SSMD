cca176b6a3955aeda1ef850e8a793016fff72671
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2026-05-26 08:32:34 +0200

    Straßenerkennung)

--- Assets/Fahrsimulation/Scripts/Project/OnTrack.cs
+++ Assets/Fahrsimulation/Scripts/Project/OnTrack.cs
@@ -6,7 +6,14 @@ public class OnTrack : MonoBehaviour
     public TextMeshProUGUI Text;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Ground"))
+        if (
+    !other.CompareTag("Vorrangstraße") &&
+    !other.CompareTag("links") &&
+    !other.CompareTag("rechts") &&
+    !other.CompareTag("gerade") &&
+    !other.CompareTag("Linie") &&
+    !other.CompareTag("Strasse")
+        )
         {
             Text.text = "Du befindest dich nicht mehr auf der Spur !";
 
